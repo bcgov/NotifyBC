662676417e4ee26842aaffd27a992ebe118a5481
f-w <fred.wen@gov.bc.ca>
2018-08-29 13:30:24 -0700

    auto bounce handling #38

--- spec/app/inbound-smtp-server.spec.js
+++ spec/app/inbound-smtp-server.spec.js
@@ -7,7 +7,7 @@ let smtpSvr
 let origRequest
 let port
 beforeAll(done => {
-  require('../../server/server.js')(function(err, data) {
+  require('../../server/server.js')(function (err, data) {
     app = data
     let smtpSvrImport = require('../../server/smtp-server')
     smtpSvrImport.app((err, data) => {
@@ -19,9 +19,9 @@ beforeAll(done => {
   })
 })
 
-describe('list-unsubscribe by email', function() {
+describe('list-unsubscribe by email', function () {
   let connection
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     spyOn(smtpSvr, 'onRcptTo').and.callThrough()
     spyOn(smtpSvr, 'onData').and.callThrough()
     connection = new SMTPConnection({
@@ -34,41 +34,40 @@ describe('list-unsubscribe by email', function() {
 
     parallel(
       [
-        function(cb) {
-          app.models.Subscription.create(
-            {
+        function (cb) {
+          app.models.Subscription.create({
               serviceName: 'myService',
               channel: 'email',
               userChannelId: 'bar@foo.com',
               state: 'confirmed',
               unsubscriptionCode: '12345'
             },
-            function(err, res) {
+            function (err, res) {
               cb(err, res)
             }
           )
         }
       ],
-      function(err, results) {
+      function (err, results) {
         expect(err).toBeNull()
         done()
       }
     )
   })
 
-  it('should accept valid email', function(done) {
-    spyOn(origRequest, 'get').and.callFake(function() {
+  it('should accept valid email', function (done) {
+    spyOn(origRequest, 'get').and.callFake(function () {
       let getReq = request(app).get(
-        arguments[0].url.substring(arguments[0].url.indexOf('/api'))
+        arguments[0].substring(arguments[0].indexOf('/api'))
       )
-      for (let p in arguments[0].headers) {
-        if (arguments[0].headers.hasOwnProperty(p)) {
-          getReq.set(p, arguments[0].headers[p])
+      for (let p in arguments[1].headers) {
+        if (arguments[1].headers.hasOwnProperty(p)) {
+          getReq.set(p, arguments[1].headers[p])
         }
       }
       getReq.end((err, res) => {
         expect(err).toBeNull()
-        app.models.Subscription.findById(1, function(err, data) {
+        app.models.Subscription.findById(1, function (err, data) {
           expect(data.state).toBe('deleted')
           done()
         })
@@ -76,17 +75,17 @@ describe('list-unsubscribe by email', function() {
     })
     expect(port).toBeGreaterThan(0)
     connection.connect(() => {
-      connection.send(
-        { from: 'bar@foo.com', to: 'un-1-12345@invalid.local' },
+      connection.send({
+          from: 'bar@foo.com',
+          to: 'un-1-12345@invalid.local'
+        },
         'unsubscribe',
         (err, info) => {
           expect(err).toBeNull()
           expect(info.accepted.length).toBe(1)
           expect(smtpSvr.onRcptTo).toHaveBeenCalled()
           expect(smtpSvr.onData).toHaveBeenCalled()
-          expect(origRequest.get).toHaveBeenCalledWith({
-            url:
-              'http://localhost:3000/api/subscriptions/1/unsubscribe?unsubscriptionCode=12345&userChannelId=bar%40foo.com',
+          expect(origRequest.get).toHaveBeenCalledWith('http://localhost:3000/api/subscriptions/1/unsubscribe?unsubscriptionCode=12345&userChannelId=bar%40foo.com', {
             headers: {
               /*jshint camelcase: false */
               is_anonymous: true
@@ -97,10 +96,12 @@ describe('list-unsubscribe by email', function() {
     })
   })
 
-  it('should reject email of invalid local-part pattern', function(done) {
+  it('should reject email of invalid local-part pattern', function (done) {
     connection.connect(() => {
-      connection.send(
-        { from: 'bar@foo.com', to: 'undo-1-12345@invalid.local' },
+      connection.send({
+          from: 'bar@foo.com',
+          to: 'undo-1-12345@invalid.local'
+        },
         'unsubscribe',
         (err, info) => {
           expect(err.rejected.length).toBe(1)
@@ -112,10 +113,12 @@ describe('list-unsubscribe by email', function() {
     })
   })
 
-  it('should reject email of invalid domain', function(done) {
+  it('should reject email of invalid domain', function (done) {
     connection.connect(() => {
-      connection.send(
-        { from: 'bar@foo.com', to: 'un-1-12345@valid.local' },
+      connection.send({
+          from: 'bar@foo.com',
+          to: 'un-1-12345@valid.local'
+        },
         'unsubscribe',
         (err, info) => {
           expect(err.rejected.length).toBe(1)
