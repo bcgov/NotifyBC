b50fdc40aa908d463afd6b69eb657e282a84d63c
f-w <fred.wen@gov.bc.ca>
2019-01-03 15:49:59 -0800

    added subscription model

---
+++ lb4/src/models/subscription.model.ts
@@ -0,0 +1,62 @@
+import { Entity, model, property } from '@loopback/repository';
+
+@model({ settings: { strict: false } })
+export class Subscription extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  serviceName: string
+
+  @property({
+    type: 'string',
+    required: true,
+    default: 'email',
+  })
+  channel: string
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userChannelId: string
+
+  @property({
+    type: 'string',
+    required: true,
+    default: 'unconfirmed',
+  })
+  state: string
+
+  @property({
+    type: 'object',
+    description: 'Contains email template, a boolean field to indicate whether to send confirmation message, confirmation code regex or encrypted confirmation code'
+  })
+  confirmationRequest?: object
+
+  @property({
+    type: 'string',
+  })
+  userId?: string
+
+  @property({
+    type: 'string'
+  })
+  broadcastPushNotificationFilter?: string
+
+  @property({
+    type: 'date',
+    default: '$now',
+  })
+  created?: string
+
+  @property({
+    type: 'date',
+    default: '$now',
+  })
+  updated?: string
+
+  constructor(data?: Partial<Subscription>) {
+    super(data);
+  }
+}
