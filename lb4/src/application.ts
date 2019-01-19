2e6b58fb72a0c9300c5cf39f796fe670571d9e8e
f-w <fred.wen@gov.bc.ca>
2019-01-18 16:18:04 -0800

    support env-specific app configs

--- lb4/src/application.ts
+++ lb4/src/application.ts
@@ -1,23 +1,44 @@
-import { BootMixin } from '@loopback/boot'
-import { ApplicationConfig } from '@loopback/core'
+import {BootMixin} from '@loopback/boot'
+import {ApplicationConfig} from '@loopback/core'
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer'
-import { RepositoryMixin } from '@loopback/repository'
-import { RestApplication } from '@loopback/rest'
-import { ServiceMixin } from '@loopback/service-proxy'
+import {RepositoryMixin} from '@loopback/repository'
+import {RestApplication} from '@loopback/rest'
+import {ServiceMixin} from '@loopback/service-proxy'
 import * as path from 'path'
-import { MySequence } from './sequence'
+import {MySequence} from './sequence'
 import fs = require('fs')
-
+import * as _ from 'lodash'
 
 export class NotifyBcApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
+    let configFiles = [
+      'config.json',
+      'config.js',
+      'config.local.json',
+      'config.local.js',
+    ]
+    if (process.env.NODE_ENV) {
+      configFiles = configFiles.concat([
+        `config.${process.env.NODE_ENV}.json`,
+        `config.${process.env.NODE_ENV}.js`,
+      ])
+    }
+    for (const configFile of configFiles) {
+      const f = path.join(__dirname, configFile)
+      if (fs.existsSync(f)) {
+        _.mergeWith(options, require(f), (t, s) => {
+          if (_.isArray(t)) {
+            return s
+          }
+        })
+      }
+    }
     super(options)
-
     // Set up the custom sequence
     this.sequence(MySequence)
 
@@ -32,7 +53,10 @@ export class NotifyBcApplication extends BootMixin(
 
     let dsFiles = ['db.datasource.local.json', 'db.datasource.local.js']
     if (process.env.NODE_ENV) {
-      dsFiles = dsFiles.concat([`db.datasource.${process.env.NODE_ENV}.json`, `db.datasource.${process.env.NODE_ENV}.js`])
+      dsFiles = dsFiles.concat([
+        `db.datasource.${process.env.NODE_ENV}.json`,
+        `db.datasource.${process.env.NODE_ENV}.js`,
+      ])
     }
     for (const dsFile of dsFiles) {
       const f = path.join(__dirname, 'datasources', dsFile)
--- lb4/upgradeNotes.md
+++ lb4/upgradeNotes.md
@@ -1,1 +1,23 @@
-* move/rename */server/datasources.*.(json|js) to */src/datasources/db.datasource.*.(json|js)*
+- move/rename /server/datasources.\*.(json|js) to /src/datasources/db.datasource.\*.(json|js)
+- move /server/config.\*.(json|js) to /src/config.\*.(json|js)
+- move following properties in file _config.\*.(json|js)_
+
+  ```
+  {
+    "restApiRoot": "/api",
+    "host": "0.0.0.0",
+    "port": 3000
+  }
+  ```
+
+  to _rest_ property
+
+  ```
+  {
+    "rest":{
+      "restApiRoot": "/api",
+      "host": "0.0.0.0",
+      "port": 3000
+    }
+  }
+  ```
