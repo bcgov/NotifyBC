0f87f42fc22f39dc4c6b37ee05c6749c1c0d096d
f-w <fred.wen@gov.bc.ca>
2020-12-12 18:14:14 -0800

    handle dropdown list change event

--- docs/.vuepress/components/versions.vue
+++ docs/.vuepress/components/versions.vue
@@ -1,7 +1,7 @@
 <template>
-  <span class="nav-item">
+  <span class="nav-item" v-if="options && options.length > 0">
     Version:
-    <select v-model="selected">
+    <select v-model="selected" @change="onChange">
       <option v-for="option in options" :value="option.value">
         {{ option.text }}
       </option>
@@ -23,15 +23,31 @@ export default {
       const res = await Axios.get('/NotifyBC/versions.json');
       this.options = res.data;
       const path = window.location.pathname.toLowerCase();
-      const versionIdx = path.indexOf('/version/');
-      if (versionIdx >= 0) {
-        const start = versionIdx + 9;
+      if (path.startsWith('/notifybc/version/')) {
+        const start = 18;
         const end = path.indexOf('/', start);
         this.selected = path.substring(start, end);
       } else {
         this.selected = 'main';
       }
     } catch (ex) {}
+  },
+  methods: {
+    onChange(event) {
+      const targetVersionPath =
+        this.selected === 'main' ? '' : `/version/${this.selected}`;
+      const path = window.location.pathname.toLowerCase();
+      let startIdx = 9;
+      const versionIdx = path.indexOf('/version/');
+      if (versionIdx >= 0) {
+        startIdx = versionIdx + 9;
+      }
+      const endIdx = path.indexOf('/', startIdx);
+      window.location.pathname =
+        window.location.pathname.substring(0, 9) +
+        targetVersionPath +
+        window.location.pathname.substring(endIdx);
+    }
   }
 };
 </script>
