44884ad0916d40c92807db8a3e0a425192962095
f-w <fred.wen@gov.bc.ca>
2021-02-07 16:04:50 -0800

    added vue-router

--- client-new/src/main.js
+++ client-new/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 new Vue({
   store,
   vuetify,
+  router,
   render: h => h(App),
 }).$mount('#app');
