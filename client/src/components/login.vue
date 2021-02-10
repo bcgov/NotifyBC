7134adc7f9143961f222fd081a110d66c43e95b2
f-w <fred.wen@gov.bc.ca>
2021-02-10 08:19:36 -0800

    extracted access token to login component

---
+++ client/src/components/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar-items class="center-text">
+    <div class="mr-1">Access Token</div>
+    <v-text-field
+      dark
+      single-line
+      hide-details
+      v-model="accessToken"
+    ></v-text-field>
+  </v-toolbar-items>
+</template>
+
+<script>
+export default {
+  computed: {
+    accessToken: {
+      get() {
+        return this.$store.state.accessToken;
+      },
+      set(value) {
+        this.$store.commit('setAccessToken', value);
+      },
+    },
+  },
+};
+</script>
