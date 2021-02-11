1387e4b6a087be1c18efc3babed9ad522d7245a6
f-w <fred.wen@gov.bc.ca>
2021-02-11 14:50:05 -0800

    copy code block to clipboard

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -121,5 +121,9 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom']
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    '@dovyp/vuepress-plugin-clipboard-copy'
+  ]
 };
