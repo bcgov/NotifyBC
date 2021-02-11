a134a7c87d0011da0272a7543e55c5cf6a0e48c6
f-w <fred.wen@gov.bc.ca>
2021-02-11 10:53:33 -0800

    make sure summernote destroyed correctly

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
 import store from './store';
+require('bootstrap-webpack!@/../config/bootstrap.config.js');
 
 Vue.config.productionTip = false;
 
