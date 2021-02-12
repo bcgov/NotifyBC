7639502fd585fcf9ea9cab46cce4eb2214847a46
f-w <fred.wen@gov.bc.ca>
2021-02-12 15:27:48 -0800

    applied license header

--- src/interceptors/index.ts
+++ src/interceptors/index.ts
@@ -1,3 +1,18 @@
+// Copyright 2016-present Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export * from './access-token-before-create.interceptor';
 export * from './administrator-before-save.interceptor';
 export * from './configuration-before-save.interceptor';
 export * from './notification-access.interceptor';
@@ -5,4 +20,3 @@ export * from './repository-before-save.interceptor';
 export * from './subscription-access.interceptor';
 export * from './subscription-after-remote.interceptor';
 export * from './subscription-before-save.interceptor';
-export * from './access-token-before-create.interceptor';
