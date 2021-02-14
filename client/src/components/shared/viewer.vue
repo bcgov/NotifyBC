24e106056937bfe807ed4eccbd0cfdfddc9601f6
f-w <fred.wen@gov.bc.ca>
2021-02-14 09:44:23 -0800

    added license header

--- client/src/components/shared/viewer.vue
+++ client/src/components/shared/viewer.vue
@@ -1,15 +1,30 @@
+<!--
+ Copyright 2016-present Province of British Columbia
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ -->
 <template>
   <div>
-    <pre>{{JSON.stringify(item, null, 2)}}</pre>
+    <pre>{{ JSON.stringify(item, null, 2) }}</pre>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['item']
-}
+  props: ['item'],
+};
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
 div {
   display: flex;
 }
