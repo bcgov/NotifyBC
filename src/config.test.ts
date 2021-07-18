91bfd4ab46dfe3036d065b4e78a0ab810b07c5da
f-w <fred.wen@gov.bc.ca>
2021-07-18 16:02:45 -0700

    Grouped smtp and inboundSmtpServer config to email

--- src/config.test.ts
+++ src/config.test.ts
@@ -21,9 +21,26 @@ module.exports = {
   adminIps: [],
   httpHost: '',
   internalHttpHost: '',
-  smtp: {
-    host: 'invalid.local',
-    port: 465,
+  email: {
+    smtp: {
+      host: 'invalid.local',
+      port: 465,
+    },
+    inboundSmtpServer: {
+      enabled: true,
+      domain: 'invalid.local',
+      listeningSmtpPort: 0,
+      apiUrlPrefix: null,
+      bounce: {
+        unsubThreshold: 2,
+        subjectRegex: '^Returned mail: see transcript for details',
+        failedRecipientRegex:
+          '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
+      },
+      options: {
+        secure: true,
+      },
+    },
   },
   sms: {
     provider: 'swift',
@@ -64,21 +81,6 @@ module.exports = {
           },
         ],
       },
-    },
-  },
-  inboundSmtpServer: {
-    enabled: true,
-    domain: 'invalid.local',
-    listeningSmtpPort: 0,
-    apiUrlPrefix: null,
-    bounce: {
-      unsubThreshold: 2,
-      subjectRegex: '^Returned mail: see transcript for details',
-      failedRecipientRegex:
-        '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
-    },
-    options: {
-      secure: true,
     },
   },
 };
--- src/config.ts
+++ src/config.ts
@@ -40,9 +40,21 @@ const config: AnyObject = {
   legacyExplorer: false,
   adminIps: ['127.0.0.1'],
   siteMinderReverseProxyIps: ['127.0.0.1'],
-  defaultSmtp: {
-    direct: true,
-    name: 'localhost',
+  email: {
+    defaultSmtp: {
+      direct: true,
+      name: 'localhost',
+    },
+    inboundSmtpServer: {
+      enabled: true,
+      bounce: {
+        unsubThreshold: 5,
+        subjectRegex: '',
+        smtpStatusCodeRegex: '5\\.\\d{1,3}\\.\\d{1,3}',
+        failedRecipientRegex:
+          '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
+      },
+    },
   },
   sms: {
     provider: 'twilio',
@@ -157,16 +169,6 @@ const config: AnyObject = {
     reDispatchBroadcastPushNotifications: {
       // minutely
       timeSpec: '0 * * * * *',
-    },
-  },
-  inboundSmtpServer: {
-    enabled: true,
-    bounce: {
-      unsubThreshold: 5,
-      subjectRegex: '',
-      smtpStatusCodeRegex: '5\\.\\d{1,3}\\.\\d{1,3}',
-      failedRecipientRegex:
-        '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
     },
   },
   tls: {},
