411ee48162a3b078dc75cd66dfad56fc48b12330
f-w <fred.wen@gov.bc.ca>
2021-07-19 13:27:05 -0700

    moved notification.handleListUnsubscribeByEmail

--- src/config.ts
+++ src/config.ts
@@ -56,6 +56,9 @@ const config: AnyObject = {
       failedRecipientRegex:
         '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
     },
+    listUnsubscribeByEmail: {
+      enabled: true,
+    },
   },
   sms: {
     provider: 'twilio',
@@ -136,7 +139,6 @@ const config: AnyObject = {
     },
   },
   notification: {
-    handleListUnsubscribeByEmail: true,
     broadcastSubscriberChunkSize: 1000,
     broadcastSubRequestBatchSize: 10,
     guaranteedBroadcastPushDispatchProcessing: true,
