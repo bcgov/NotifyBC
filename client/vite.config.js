1ec3de2535342e6b2021a3b812ef3045ad2df204
f-w <fred.wen@gov.bc.ca>
2023-07-08 14:49:13 -0700

    define global var same way as webpack

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,68 +7,83 @@ import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
 import fs from 'fs';
 import {globSync} from 'fast-glob';
+import ejs from 'ejs';
+
 const NotifyBcApplication = require('../dist/application').NotifyBcApplication;
 const app = new NotifyBcApplication();
 const proxyProto = app.options.tls.enabled ? 'https' : 'http';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {transformAssetUrls},
-    }),
-    // glob inject css
-    {
-      load(id) {
-        if (id.endsWith('.vue')) {
-          const source = fs
-            .readFileSync(id)
-            .toString()
-            .replace(/inject-css:\s*'([^']+)';/g, replace => {
-              const pattern = replace.match(/'([^']+)/)[1];
-              return globSync(pattern, {absolute: true})
-                .map(file => fs.readFileSync(file))
-                .join('\n');
-            });
-          return source;
-        }
+export default ({mode}) => {
+  let config = {
+    plugins: [
+      vue({
+        template: {transformAssetUrls},
+      }),
+      // glob inject css
+      {
+        load(id) {
+          if (id.endsWith('.vue')) {
+            const source = fs
+              .readFileSync(id)
+              .toString()
+              .replace(/inject-css:\s*'([^']+)';/g, replace => {
+                const pattern = replace.match(/'([^']+)/)[1];
+                return globSync(pattern, {absolute: true})
+                  .map(file => fs.readFileSync(file))
+                  .join('\n');
+              });
+            return source;
+          }
+        },
+      },
+      inject({
+        // => that should be first under plugins array
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    define: {
+      'process.env': {},
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    },
+    server: {
+      port: 8000,
+      open: '/',
+      proxy: {
+        '^/api/': {
+          target: proxyProto + '://127.0.0.1:3000',
+          changeOrigin: true,
+        },
+        '^/oauth2-redirect.html': {
+          target: proxyProto + '://127.0.0.1:3000',
+        },
       },
     },
-    inject({
-      // => that should be first under plugins array
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: {
-    'process.env': {},
-    apiUrlPrefix: JSON.stringify(app.options.restApiRoot),
-    oidcAuthority: JSON.stringify(
-      app.options.oidc && app.options.oidc.discoveryUrl,
-    ),
-    oidcClientId: JSON.stringify(app.options.oidc && app.options.oidc.clientId),
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
-  },
-  server: {
-    port: 8000,
-    open: '/',
-    proxy: {
-      '^/api/': {
-        target: proxyProto + '://127.0.0.1:3000',
-        changeOrigin: true,
-      },
-      '^/oauth2-redirect.html': {
-        target: proxyProto + '://127.0.0.1:3000',
-      },
-    },
-  },
-});
+  };
+
+  if (mode === 'development') {
+    const htmlPlugin = () => {
+      return {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          return ejs.render(html, {
+            apiUrlPrefix: app.options.restApiRoot,
+            oidcAuthority: app.options.oidc && app.options.oidc.discoveryUrl,
+            oidcClientId: app.options.oidc && app.options.oidc.clientId,
+          });
+        },
+      };
+    };
+    config.plugins.push(htmlPlugin());
+  }
+  return defineConfig(config);
+};
