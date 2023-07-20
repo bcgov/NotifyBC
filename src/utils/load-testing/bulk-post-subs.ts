7208f04f6d727cfec4f5d29441c4469ae33e7bfc
f-w <fred.wen@gov.bc.ca>
2023-07-19 17:14:50 -0700

    replaced node-getopt with commander in bulk-post

--- src/utils/load-testing/bulk-post-subs.ts
+++ src/utils/load-testing/bulk-post-subs.ts
@@ -12,51 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {Command} from 'commander';
 (function () {
   const axios = require('axios');
   const queue = require('async/queue');
-  const getOpt = require('node-getopt')
-    .create([
-      [
-        'a',
-        'api-url-prefix=<string>',
-        'api url prefix. default to http://localhost:3000/api',
-      ],
-      ['c', 'channel=<string>', 'channel. default to email'],
-      ['s', 'service-name=<string>', 'service name. default to load'],
-      [
-        'n',
-        'number-of-subscribers=<int>',
-        'number of subscribers. positive integer. default to 1000',
-      ],
-      [
-        'f',
-        'broadcast-push-notification-filter=<string>',
-        "broadcast push notification filter. default to \"contains_ci(title,'vancouver') || contains_ci(title,'victoria')\"",
-      ],
-      ['h', 'help', 'display this help'],
-    ])
-    .bindHelp(
-      'Usage: node ' +
-        process.argv[1] +
-        ' [Options] <userChannleId> \n[Options]:\n[[OPTIONS]]',
-    );
-  const args = getOpt.parseSystem();
-  if (args.argv.length !== 1) {
-    console.error('invalid arguments');
-    getOpt.showHelp();
-    process.exit(1);
-  }
-  const apiUrlPrefix =
-    args.options['api-url-prefix'] || 'http://localhost:3000/api';
-  const serviceName = args.options['service-name'] || 'load';
-  const numberOfSubscribers =
-    parseInt(args.options['number-of-subscribers']) || 1000;
-  const channel = args.options['channel'] || 'email';
-  const broadcastPushNotificationFilter =
-    args.options['broadcast-push-notification-filter'] ||
-    "contains_ci(title,'vancouver') || contains_ci(title,'victoria')";
-  const userChannelId = args.argv[0];
+  const program = new Command();
+  program
+    .name(`node ${process.argv[1]}`)
+    .argument('<string>', 'userChannelId')
+    .option(
+      '-a, --api-url-prefix <string>',
+      'api url prefix',
+      'http://127.0.0.1:3000/api',
+    )
+    .option('-c, --channel <string>', 'channel', 'email')
+    .option('-s, --service-name <string>', 'service name', 'load')
+    .option(
+      '-n, --number-of-subscribers <int>',
+      'number of subscribers. positive integer.',
+      '1000',
+    )
+    .option(
+      '-f, --broadcast-push-notification-filter <string>',
+      'broadcast push notification filter',
+      "contains_ci(title,'vancouver') || contains_ci(title,'victoria')",
+    )
+    .showHelpAfterError();
+  program.parse();
+  const opts = program.opts();
+  const apiUrlPrefix = opts.apiUrlPrefix;
+  const serviceName = opts.serviceName;
+  const numberOfSubscribers = parseInt(opts.numberOfSubscribers);
+  const channel = opts.channel;
+  const broadcastPushNotificationFilter = opts.broadcastPushNotificationFilter;
+  const userChannelId = program.args[0];
   let successCnt = 0;
   const q = queue(function (index: any, cb: () => void) {
     const options = {
@@ -89,14 +78,13 @@
         cb();
       });
   }, 100);
+  q.drain(function () {
+    console.log(`success count ${successCnt}`);
+    process.exit(0);
+  });
   let i = 0;
   /*jshint loopfunc:true */
   while (i < numberOfSubscribers) {
     q.push(i++);
   }
-
-  q.drain(function () {
-    console.log(`success count ${successCnt}`);
-    process.exit(0);
-  });
 })();
