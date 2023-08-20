cf86e078090a56d91064274e6f3c5fe35d293563
f-w <fred.wen@gov.bc.ca>
2023-08-20 10:31:29 -0700

    swapped nest and lb code

---
+++ notify-bc-lb/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '@loopback/eslint-config',
+  rules: {
+    'no-empty': ['error', {allowEmptyCatch: true}],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-expressions': [
+      'error',
+      {allowShortCircuit: true, allowTernary: true},
+    ],
+  },
+};
