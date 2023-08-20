389f6955157b870cc88d5620e6aed15e383a85e6
f-w <fred.wen@gov.bc.ca>
2023-08-20 10:31:29 -0700

    swapped nest and lb code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,25 @@
 module.exports = {
-  extends: '@loopback/eslint-config',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'no-empty': ['error', {allowEmptyCatch: true}],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-unused-expressions': [
-      'error',
-      {allowShortCircuit: true, allowTernary: true},
-    ],
   },
 };
