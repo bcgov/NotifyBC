4d2ba40ab7f2824c5935e9edfdc56de7e9f9a099
f-w <fred.wen@gov.bc.ca>
2023-08-26 10:48:13 -0700

    nest g res notifications api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigurationsModule } from './api/configurations/configurations.module';
+import { NotificationsModule } from './api/notifications/notifications.module';
 
 @Module({
-  imports: [ConfigModule, ConfigurationsModule],
+  imports: [ConfigModule, ConfigurationsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
