f72674e3886b4162aa4c95d85125c58972ffe2ca
f-w <fred.wen@gov.bc.ca>
2023-08-26 15:39:25 -0700

    get /api/configurations works

--- src/api/base.entity.ts
+++ src/api/base.entity.ts
@@ -1,1 +1,14 @@
-export class BaseEntity {}
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema()
+export class BaseEntity {
+  @Prop(Types.ObjectId)
+  id?: string;
+
+  @Prop({ default: Date.now })
+  created?: Date;
+
+  @Prop({ default: Date.now })
+  updated?: Date;
+}
--- src/api/configurations/configurations.module.ts
+++ src/api/configurations/configurations.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationsService } from './configurations.service';
 import { ConfigurationsController } from './configurations.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Configuration,
+  ConfigurationSchema,
+} from './entities/configuration.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Configuration.name, schema: ConfigurationSchema },
+    ]),
+  ],
   controllers: [ConfigurationsController],
   providers: [ConfigurationsService],
 })
