19c33e37bc444516efb70f28c0d3ce352767b5aa
f-w <fred.wen@gov.bc.ca>
2023-09-03 06:50:50 -0700

    implemented crud for config api

--- src/api/configurations/configurations.controller.ts
+++ src/api/configurations/configurations.controller.ts
@@ -6,8 +6,10 @@ import {
   Param,
   Patch,
   Post,
+  Put,
 } from '@nestjs/common';
-import { FilterQuery } from 'mongoose';
+import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { FilterQuery, ObjectId } from 'mongoose';
 import { LoopbackFilterDto } from '../common/dto/loopback-filter.dto';
 import { JsonQuery } from '../common/json-query.decorator';
 import { ConfigurationsService } from './configurations.service';
@@ -16,6 +18,8 @@ import { UpdateConfigurationDto } from './dto/update-configuration.dto';
 import { Configuration } from './entities/configuration.entity';
 
 @Controller('configurations')
+@ApiTags('configuration')
+@ApiExtraModels(Configuration)
 export class ConfigurationsController {
   constructor(private readonly configurationsService: ConfigurationsService) {}
 
@@ -37,21 +41,37 @@ export class ConfigurationsController {
     return this.configurationsService.findAll(filter);
   }
 
+  @Patch()
+  updateAll(
+    @Body() updateConfigurationDto: UpdateConfigurationDto,
+    @JsonQuery('where') where?: FilterQuery<Configuration>,
+  ) {
+    return this.configurationsService.updateAll(updateConfigurationDto, where);
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.configurationsService.findOne(+id);
+  findOne(@Param('id') id: ObjectId) {
+    return this.configurationsService.findOne(id);
   }
 
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id') id: ObjectId,
     @Body() updateConfigurationDto: UpdateConfigurationDto,
   ) {
-    return this.configurationsService.update(+id, updateConfigurationDto);
+    return this.configurationsService.update(id, updateConfigurationDto);
+  }
+
+  @Put(':id')
+  replaceById(
+    @Param('id') id: ObjectId,
+    @Body() updateConfigurationDto: UpdateConfigurationDto,
+  ) {
+    return this.configurationsService.replaceById(id, updateConfigurationDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.configurationsService.remove(+id);
+  remove(@Param('id') id: ObjectId) {
+    return this.configurationsService.remove(id);
   }
 }
