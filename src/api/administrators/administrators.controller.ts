3465209cb624527f63a736e44e2818e1af763aa3
f-w <fred.wen@gov.bc.ca>
2023-09-08 10:58:26 -0700

    crud for administrator api

--- src/api/administrators/administrators.controller.ts
+++ src/api/administrators/administrators.controller.ts
@@ -1,11 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { FilterQuery } from 'mongoose';
+import { ApiWhereJsonQuery, JsonQuery } from '../common/json-query.decorator';
 import { AdministratorsService } from './administrators.service';
 import { CreateAdministratorDto } from './dto/create-administrator.dto';
 import { UpdateAdministratorDto } from './dto/update-administrator.dto';
+import { Administrator } from './entities/administrator.entity';
 
 @Controller('administrators')
+@ApiTags('administrator')
+@ApiExtraModels(Administrator)
 export class AdministratorsController {
   constructor(private readonly administratorsService: AdministratorsService) {}
+
+  @Get('count')
+  @ApiWhereJsonQuery()
+  @ApiOkResponse({
+    description: 'Administrator count',
+    type: Number,
+  })
+  count(@JsonQuery('where') where?: FilterQuery<Administrator>) {
+    return this.administratorsService.count(where);
+  }
 
   @Post()
   create(@Body() createAdministratorDto: CreateAdministratorDto) {
@@ -23,7 +47,10 @@ export class AdministratorsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdministratorDto: UpdateAdministratorDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateAdministratorDto: UpdateAdministratorDto,
+  ) {
     return this.administratorsService.update(+id, updateAdministratorDto);
   }
 
--- src/api/administrators/administrators.module.ts
+++ src/api/administrators/administrators.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AdministratorsService } from './administrators.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdministratorsController } from './administrators.controller';
+import { AdministratorsService } from './administrators.service';
+import {
+  Administrator,
+  AdministratorSchema,
+} from './entities/administrator.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Administrator.name, schema: AdministratorSchema },
+    ]),
+  ],
   controllers: [AdministratorsController],
   providers: [AdministratorsService],
 })
