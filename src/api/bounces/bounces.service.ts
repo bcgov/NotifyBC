3a1dfd05686e5a572ff0fdaa80dcc94e5c4ac1a1
f-w <fred.wen@gov.bc.ca>
2023-09-10 15:58:14 -0700

    move most service methods to base

--- src/api/bounces/bounces.service.ts
+++ src/api/bounces/bounces.service.ts
@@ -1,48 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model } from 'mongoose';
-import { CreateBounceDto } from './dto/create-bounce.dto';
-import { UpdateBounceDto } from './dto/update-bounce.dto';
+import { Model } from 'mongoose';
+import { BaseService } from '../common/base.service';
 import { Bounce } from './entities/bounce.entity';
 
 @Injectable()
-export class BouncesService {
+export class ConfigurationsService extends BaseService<Bounce> {
   constructor(
     @InjectModel(Bounce.name)
-    private bounceModel: Model<Bounce>,
-  ) {}
-
-  count(filter?: FilterQuery<Bounce>) {
-    return this.bounceModel.count(filter).exec();
-  }
-
-  replaceById(_id: string, updateBounceDto: UpdateBounceDto) {
-    return this.bounceModel.findOneAndReplace({ _id }, updateBounceDto).exec();
-  }
-
-  create(createBounceDto: CreateBounceDto) {
-    const createdConfiguration = new this.bounceModel(createBounceDto);
-    return createdConfiguration.save();
-  }
-
-  findAll(filter: any = {}) {
-    const { where, fields, include, order, ...rest } = filter;
-    return this.bounceModel.find(where, fields, rest).sort(order).exec();
-  }
-
-  findOne(id: string) {
-    return this.bounceModel.findById(id).exec();
-  }
-
-  update(id: string, updateBounceDto: UpdateBounceDto) {
-    return this.bounceModel.findByIdAndUpdate(id, updateBounceDto).exec();
-  }
-
-  remove(id: string) {
-    this.bounceModel.findByIdAndRemove(id).exec();
-  }
-
-  updateAll(updateDto: UpdateBounceDto, filter?: FilterQuery<Bounce>) {
-    return this.bounceModel.updateMany(filter, updateDto).exec();
+    model: Model<Bounce>,
+  ) {
+    super(model);
   }
 }
--- src/api/common/base.service.ts
+++ src/api/common/base.service.ts
@@ -1,1 +1,62 @@
-export class BaseService {}
+import { compact } from 'lodash';
+import { FilterQuery, Model } from 'mongoose';
+export class BaseService<T> {
+  constructor(private model: Model<T>) {}
+
+  create(createDto) {
+    const createdConfiguration = new this.model(createDto);
+    return createdConfiguration.save();
+  }
+
+  count(filter?: FilterQuery<T>) {
+    return this.model.count(filter).exec();
+  }
+
+  findAll(filter: any = {}) {
+    const { where, fields, order, skip, limit } = filter;
+    return this.model
+      .aggregate(
+        compact([
+          {
+            $addFields: {
+              id: { $toString: '$_id' },
+            },
+          },
+          where && {
+            $match: where,
+          },
+          order && {
+            $sort: order,
+          },
+          skip && {
+            $skip: skip,
+          },
+          limit && { $limit: limit },
+          {
+            $project: { ...fields, _id: false },
+          },
+        ]),
+      )
+      .exec();
+  }
+
+  updateAll(updateDto, filter?: FilterQuery<T>) {
+    return this.model.updateMany(filter, updateDto).exec();
+  }
+
+  findOne(id: string) {
+    return this.model.findById(id).exec();
+  }
+
+  update(id: string, updateDto) {
+    return this.model.findByIdAndUpdate(id, updateDto).exec();
+  }
+
+  replaceById(_id: string, updateDto) {
+    return this.model.findOneAndReplace({ _id }, updateDto).exec();
+  }
+
+  remove(id: string) {
+    this.model.findByIdAndRemove(id).exec();
+  }
+}
