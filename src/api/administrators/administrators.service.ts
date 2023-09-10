b662fcd81ab6e2024a1479ec8ed088c5ce52bff8
f-w <fred.wen@gov.bc.ca>
2023-09-10 15:58:14 -0700

    move most service methods to base

--- src/api/administrators/administrators.service.ts
+++ src/api/administrators/administrators.service.ts
@@ -1,38 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model } from 'mongoose';
-import { CreateAdministratorDto } from './dto/create-administrator.dto';
-import { UpdateAdministratorDto } from './dto/update-administrator.dto';
+import { Model } from 'mongoose';
+import { BaseService } from '../common/base.service';
 import { Administrator } from './entities/administrator.entity';
 
 @Injectable()
-export class AdministratorsService {
+export class AdministratorsService extends BaseService<Administrator> {
   constructor(
     @InjectModel(Administrator.name)
-    private administratorModel: Model<Administrator>,
-  ) {}
-
-  create(createAdministratorDto: CreateAdministratorDto) {
-    return 'This action adds a new administrator';
-  }
-
-  count(filter?: FilterQuery<Administrator>) {
-    return this.administratorModel.count(filter).exec();
-  }
-
-  findAll() {
-    return `This action returns all administrators`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} administrator`;
-  }
-
-  update(id: number, updateAdministratorDto: UpdateAdministratorDto) {
-    return `This action updates a #${id} administrator`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} administrator`;
+    model: Model<Administrator>,
+  ) {
+    super(model);
   }
 }
--- src/api/configurations/configurations.service.ts
+++ src/api/configurations/configurations.service.ts
@@ -1,82 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { compact } from 'lodash';
-import { FilterQuery, Model } from 'mongoose';
-import { CreateConfigurationDto } from './dto/create-configuration.dto';
-import { UpdateConfigurationDto } from './dto/update-configuration.dto';
+import { Model } from 'mongoose';
+import { BaseService } from '../common/base.service';
 import { Configuration } from './entities/configuration.entity';
 @Injectable()
-export class ConfigurationsService {
+export class ConfigurationsService extends BaseService<Configuration> {
   constructor(
     @InjectModel(Configuration.name)
-    private configurationModel: Model<Configuration>,
-  ) {}
-
-  create(createConfigurationDto: CreateConfigurationDto) {
-    const createdConfiguration = new this.configurationModel(
-      createConfigurationDto,
-    );
-    return createdConfiguration.save();
-  }
-
-  count(filter?: FilterQuery<Configuration>) {
-    return this.configurationModel.count(filter).exec();
-  }
-
-  findAll(filter: any = {}) {
-    const { where, fields, include, order, skip, limit, ...rest } = filter;
-    return this.configurationModel
-      .aggregate(
-        compact([
-          {
-            $addFields: {
-              id: { $toString: '$_id' },
-            },
-          },
-          where && {
-            $match: where,
-          },
-          order && {
-            $sort: order,
-          },
-          skip && {
-            $skip: skip,
-          },
-          limit && { $limit: limit },
-          {
-            $project: { ...fields, _id: false },
-          },
-        ]),
-      )
-      .exec();
-  }
-
-  updateAll(
-    updateConfigurationDto: UpdateConfigurationDto,
-    filter?: FilterQuery<Configuration>,
+    model: Model<Configuration>,
   ) {
-    return this.configurationModel
-      .updateMany(filter, updateConfigurationDto)
-      .exec();
-  }
-
-  findOne(id: string) {
-    return this.configurationModel.findById(id).exec();
-  }
-
-  update(id: string, updateConfigurationDto: UpdateConfigurationDto) {
-    return this.configurationModel
-      .findByIdAndUpdate(id, updateConfigurationDto)
-      .exec();
-  }
-
-  replaceById(_id: string, updateConfigurationDto: UpdateConfigurationDto) {
-    return this.configurationModel
-      .findOneAndReplace({ _id }, updateConfigurationDto)
-      .exec();
-  }
-
-  remove(id: string) {
-    this.configurationModel.findByIdAndRemove(id).exec();
+    super(model);
   }
 }
