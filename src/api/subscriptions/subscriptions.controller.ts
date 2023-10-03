9574f4388c3dedf606685b6790838aa6e4756209
f-w <fred.wen@gov.bc.ca>
2023-10-03 11:25:37 -0700

    ported SubscriptionAfterRemoteInterceptor

--- src/api/subscriptions/subscriptions.controller.ts
+++ src/api/subscriptions/subscriptions.controller.ts
@@ -1,47 +1,39 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { SubscriptionAfterRemoteInterceptor } from './subscription-after-remote.interceptor';
 import { SubscriptionsService } from './subscriptions.service';
 
 @Controller('subscriptions')
 @ApiTags('subscription')
+@UseInterceptors(SubscriptionAfterRemoteInterceptor)
 export class SubscriptionsController {
   constructor(private readonly subscriptionsService: SubscriptionsService) {}
 
-  @Post()
-  create(@Body() createSubscriptionDto: CreateSubscriptionDto) {
-    return this.subscriptionsService.create(createSubscriptionDto);
-  }
+  // @Post()
+  // create(@Body() createSubscriptionDto: CreateSubscriptionDto) {
+  //   return this.subscriptionsService.create(createSubscriptionDto);
+  // }
 
   @Get()
   findAll() {
     return this.subscriptionsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.subscriptionsService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.subscriptionsService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateSubscriptionDto: UpdateSubscriptionDto,
-  ) {
-    return this.subscriptionsService.update(+id, updateSubscriptionDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateSubscriptionDto: UpdateSubscriptionDto,
+  // ) {
+  //   return this.subscriptionsService.update(+id, updateSubscriptionDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.subscriptionsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.subscriptionsService.remove(+id);
+  // }
 }
--- src/api/subscriptions/subscriptions.service.ts
+++ src/api/subscriptions/subscriptions.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { BaseService } from '../common/base.service';
+import { Subscription } from './entities/subscription.entity';
 
 @Injectable()
-export class SubscriptionsService {
-  create(createSubscriptionDto: CreateSubscriptionDto) {
-    return 'This action adds a new subscription';
-  }
-
-  findAll() {
-    return `This action returns all subscriptions`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} subscription`;
-  }
-
-  update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
-    return `This action updates a #${id} subscription`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} subscription`;
+export class SubscriptionsService extends BaseService<Subscription> {
+  constructor(
+    @InjectModel(Subscription.name)
+    model: Model<Subscription>,
+  ) {
+    super(model);
   }
 }
