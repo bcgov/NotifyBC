fbc51f2feb73be7e4fa5c25959bdb3e33be57328
f-w <fred.wen@gov.bc.ca>
2023-10-07 14:28:01 -0700

    ported mailMerge

--- src/api/common/base.controller.ts
+++ src/api/common/base.controller.ts
@@ -1,8 +1,11 @@
 import { Controller } from '@nestjs/common';
 import { merge } from 'lodash';
+import pluralize from 'pluralize';
 import { AppConfigService } from 'src/config/app-config.service';
+import toSentence from 'underscore.string/toSentence';
 import { ConfigurationsService } from '../configurations/configurations.service';
 import { Configuration } from '../configurations/entities/configuration.entity';
+import { Subscription } from '../subscriptions/entities/subscription.entity';
 
 @Controller()
 export class BaseController {
@@ -10,6 +13,186 @@ export class BaseController {
     readonly appConfigService: AppConfigService,
     readonly configurationsService: ConfigurationsService,
   ) {}
+
+  mailMerge(
+    srcTxt: any,
+    subscription: Partial<Subscription>,
+    notification: Partial<Notification>,
+    req: any,
+  ): any {
+    let output = srcTxt;
+    try {
+      output = output.replace(
+        /(?<!\\){subscription_confirmation_code(?<!\\)}/gi,
+        subscription.confirmationRequest?.confirmationCode,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){service_name(?<!\\)}/gi,
+        subscription.serviceName,
+      );
+    } catch (ex) {}
+    try {
+      if (output.match(/(?<!\\){unsubscription_service_names(?<!\\)}/i)) {
+        const serviceNames = _.union(
+          [subscription.serviceName],
+          subscription.unsubscribedAdditionalServices
+            ? subscription.unsubscribedAdditionalServices.names
+            : [],
+        );
+        output = output.replace(
+          /(?<!\\){unsubscription_service_names(?<!\\)}/gi,
+          pluralize('service', serviceNames.length) +
+            ' ' +
+            toSentence(serviceNames),
+        );
+      }
+    } catch (ex) {}
+    let httpHost;
+    try {
+      if (req) {
+        httpHost = req.protocol + '://' + req.get('host');
+      }
+      if (this.appConfigService.get('httpHost')) {
+        httpHost = this.appConfigService.get('httpHost');
+      }
+      let args: any;
+      try {
+        args = req.getSync('args');
+      } catch (ex) {}
+      if (args?.data?.httpHost) {
+        httpHost = args.data.httpHost;
+      } else if (req.instance?.httpHost) {
+        httpHost = req.instance.httpHost;
+      } else if (subscription?.httpHost) {
+        httpHost = subscription.httpHost;
+      }
+      output = output.replace(/(?<!\\){http_host(?<!\\)}/gi, httpHost);
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){rest_api_root(?<!\\)}/gi,
+        this.appConfigService.get('restApiRoot'),
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){subscription_id(?<!\\)}/gi,
+        subscription.id,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){unsubscription_code(?<!\\)}/gi,
+        subscription.unsubscriptionCode,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){unsubscription_url(?<!\\)}/gi,
+        httpHost +
+          this.appConfigService.get('restApiRoot') +
+          '/subscriptions/' +
+          subscription.id +
+          '/unsubscribe?unsubscriptionCode=' +
+          subscription.unsubscriptionCode,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){unsubscription_all_url(?<!\\)}/gi,
+        httpHost +
+          this.appConfigService.get('restApiRoot') +
+          '/subscriptions/' +
+          subscription.id +
+          '/unsubscribe?unsubscriptionCode=' +
+          subscription.unsubscriptionCode +
+          '&additionalServices=_all',
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){subscription_confirmation_url(?<!\\)}/gi,
+        httpHost +
+          this.appConfigService.get('restApiRoot') +
+          '/subscriptions/' +
+          subscription.id +
+          '/verify?confirmationCode=' +
+          subscription.confirmationRequest?.confirmationCode,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){unsubscription_reversion_url(?<!\\)}/gi,
+        httpHost +
+          this.appConfigService.get('restApiRoot') +
+          '/subscriptions/' +
+          subscription.id +
+          '/unsubscribe/undo?unsubscriptionCode=' +
+          subscription.unsubscriptionCode,
+      );
+    } catch (ex) {}
+
+    // for backward compatibilities
+    try {
+      output = output.replace(
+        /(?<!\\){confirmation_code(?<!\\)}/gi,
+        subscription.confirmationRequest?.confirmationCode,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){serviceName(?<!\\)}/gi,
+        subscription.serviceName,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){restApiRoot(?<!\\)}/gi,
+        this.appConfigService.get('restApiRoot'),
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){subscriptionId(?<!\\)}/gi,
+        subscription.id,
+      );
+    } catch (ex) {}
+    try {
+      output = output.replace(
+        /(?<!\\){unsubscriptionCode(?<!\\)}/gi,
+        subscription.unsubscriptionCode,
+      );
+    } catch (ex) {}
+    // substitute all other tokens with matching data.data properties
+    const matches = output.match(/(?<!\\){.+?(?<!\\)}/g);
+    if (matches) {
+      matches.forEach(function (e: string) {
+        try {
+          const token = (e.match(/(?<!\\){(.+)(?<!\\)}/) ?? [])[1];
+          const tokenParts = token.split('::');
+          let val: string;
+          switch (tokenParts[0]) {
+            case 'subscription':
+              val = _.get(subscription.data ?? {}, tokenParts[1]);
+              break;
+            case 'notification':
+              val = _.get(notification.data ?? {}, tokenParts[1]);
+              break;
+            default:
+              val = _.get(notification.data ?? subscription.data ?? {}, token);
+          }
+          if (val) {
+            output = output.replace(e, val);
+          }
+        } catch (ex) {}
+      });
+    }
+    // unescape delimiter
+    output = output.replace(/\\{(.+?)\\}/g, '{$1}');
+    return output;
+  }
 
   async getMergedConfig(
     configName: string,
