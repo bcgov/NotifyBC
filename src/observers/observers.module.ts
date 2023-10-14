a25a1b07a3ad033abfc4b2ee8e72d8e7a7810e8e
f-w <fred.wen@gov.bc.ca>
2023-10-14 11:31:02 -0700

    ported rsa observer

--- src/observers/observers.module.ts
+++ src/observers/observers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigurationsModule } from 'src/api/configurations/configurations.module';
 import { RsaService } from './rsa.service';
 
 @Module({
-  providers: [RsaService]
+  providers: [RsaService],
+  imports: [ConfigurationsModule],
 })
 export class ObserversModule {}
--- src/observers/rsa.service.ts
+++ src/observers/rsa.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import NodeRSA from 'node-rsa';
+import { ConfigurationsService } from 'src/api/configurations/configurations.service';
+import { promisify } from 'util';
 
 @Injectable()
-export class RsaService {}
+export class RsaService implements OnApplicationBootstrap {
+  constructor(private readonly configurationsService: ConfigurationsService) {}
+
+  /**
+   * This method will be invoked when the application starts
+   */
+  async onApplicationBootstrap(): Promise<void> {
+    if (process.env.NODE_ENV === 'test') {
+      return;
+    }
+    const data = await this.configurationsService.findOne({
+      where: {
+        name: 'rsa',
+      },
+    });
+    const key = new NodeRSA();
+    if (data) {
+      key.importKey(data.value.private, 'private');
+      key.importKey(data.value.public, 'public');
+      module.exports.key = key;
+      return;
+    }
+    if (process.env.NOTIFYBC_NODE_ROLE === 'slave') {
+      await promisify(setTimeout)(5000);
+      await this.onApplicationBootstrap();
+      return;
+    }
+    // only the node with cron enabled, which is supposed to be a singleton,
+    // can generate RSA key pair by executing code below
+    key.generateKeyPair();
+    module.exports.key = key;
+    await this.configurationsService.create(
+      {
+        name: 'rsa',
+        value: {
+          private: key.exportKey('private'),
+          public: key.exportKey('public'),
+        },
+      },
+      undefined,
+    );
+    return;
+  }
+}
