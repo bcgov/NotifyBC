8208fbb1f5b23986cc816c9e6c6abe9dd58d1d46
f-w <fred.wen@gov.bc.ca>
2023-10-25 10:34:32 -0700

    ported NotificationAccessInterceptor

--- src/api/notifications/notifications.service.ts
+++ src/api/notifications/notifications.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Request } from 'express';
+import { FilterQuery, Model } from 'mongoose';
+import { Role } from 'src/auth/constants';
+import { UserProfile } from 'src/auth/dto/user-profile.dto';
 import { BaseService } from '../common/base.service';
+import { FilterDto } from '../common/dto/filter.dto';
 import { Notification } from './entities/notification.entity';
 
 @Injectable()
@@ -11,5 +15,65 @@ export class NotificationsService extends BaseService<Notification> {
     model: Model<Notification>,
   ) {
     super(model);
+  }
+
+  accessInterceptor(
+    req: Request & { user: UserProfile },
+    where: FilterQuery<Notification>,
+  ): FilterQuery<Notification> {
+    if (!req) return where;
+    if ([Role.Admin, Role.SuperAdmin].includes(req.user?.role)) return where;
+    if (req.user?.role !== Role.AuthenticatedUser)
+      throw new HttpException(undefined, HttpStatus.FORBIDDEN);
+    return {
+      $and: [
+        where || {},
+        { channel: 'inApp' },
+        {
+          $or: [
+            { isBroadcast: true },
+            {
+              $or: [
+                { userChannelId: req.user?.securityId },
+                { userId: req.user?.securityId },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  findAll(
+    req: Request & { user: UserProfile },
+    filter: FilterDto<Notification> = {},
+  ) {
+    filter.where = this.accessInterceptor(req, filter.where);
+    return super.findAll(filter);
+  }
+
+  async findOne(
+    req: Request & { user: UserProfile },
+    filter: FilterDto<Notification> = {},
+  ): Promise<Notification> {
+    filter.where = this.accessInterceptor(req, filter.where);
+    return super.findOne(filter);
+  }
+
+  updateAll(
+    updateDto,
+    where: FilterQuery<Notification> | null,
+    req: Request & { user: UserProfile },
+  ) {
+    where = this.accessInterceptor(req, where);
+    return super.updateAll(updateDto, where, req);
+  }
+
+  async removeAll(
+    req: Request & { user: UserProfile },
+    where?: FilterQuery<Notification>,
+  ) {
+    where = this.accessInterceptor(req, where);
+    return super.removeAll(where);
   }
 }
