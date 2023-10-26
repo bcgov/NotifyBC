d309c6f946360313f34f21fb87b32eb50dfa8fb3
f-w <fred.wen@gov.bc.ca>
2023-10-26 11:04:30 -0700

    added Rss service

--- src/rss/rss.module.ts
+++ src/rss/rss.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Rss, RssSchema } from './entities/rss.entity';
+import { RssService } from './rss.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Rss.name, schema: RssSchema }])],
+  providers: [RssService],
+  exports: [RssService],
 })
 export class RssModule {}
