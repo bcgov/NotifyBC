1a4f6037b252c25eb59d06230177e8bbeb061caf
f-w <fred.wen@gov.bc.ca>
2023-10-27 09:20:23 -0700

    converted cron-tasks to service

--- src/observers/observers.module.ts
+++ src/observers/observers.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationsModule } from 'src/api/configurations/configurations.module';
+import { CronTasksService } from './cron-tasks.service';
 import { IndexDBSchemaService } from './index-dbschema.service';
 import { RsaService } from './rsa.service';
 
 @Module({
-  providers: [RsaService, IndexDBSchemaService],
+  providers: [RsaService, IndexDBSchemaService, CronTasksService],
   imports: [ConfigurationsModule],
 })
 export class ObserversModule {}
