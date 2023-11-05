1b91e880f94f8689a6247bfbc178f0f8a3bc0fd8
f-w <fred.wen@gov.bc.ca>
2023-11-05 11:35:49 -0800

    ported SecuritySpecEnhancer

--- notify-bc-lb/src/services/security-spec.enhancer.ts
+++ notify-bc-lb/src/services/security-spec.enhancer.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// file ported
 import {injectable} from '@loopback/core';
 import {
   asSpecEnhancer,
