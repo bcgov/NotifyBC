645edfbe82e2804482894d0f63c5be1e854419d5
f-w <fred.wen@gov.bc.ca>
2023-11-05 15:03:16 -0800

    fix typo

--- src/config/middleware-config.service.ts
+++ src/config/middleware-config.service.ts
@@ -1,10 +1,10 @@
-import {Inject, Injectable} from '@nestjs/common';
-import {ConfigType} from './constants';
-import {get} from 'lodash';
+import { Inject, Injectable } from '@nestjs/common';
+import { get } from 'lodash';
+import { ConfigType } from './constants';
 
 @Injectable()
 export class MiddlewareConfigService {
-  constructor(@Inject(ConfigType.AppConfig) private readonly config) {}
+  constructor(@Inject(ConfigType.MiddlewareConfig) private readonly config) {}
 
   get(propName?: string, defaultVal?: any) {
     if (!propName) return this.config;
