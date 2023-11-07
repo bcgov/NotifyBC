4e346eefe9b28c81082764a4477892b6a9e62719
f-w <fred.wen@gov.bc.ca>
2023-11-07 12:12:46 -0800

    ported load-testing scripts

--- src/utils/load-testing/bulk-post-subs.ts
+++ src/utils/load-testing/bulk-post-subs.ts
@@ -1,21 +1,6 @@
-// Copyright 2016-present Province of British Columbia
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// file ported
-import {Command} from 'commander';
+import { queue } from 'async';
+import { Command } from 'commander';
 (function () {
-  const queue = require('async/queue');
   const program = new Command();
   program
     .name(`node ${process.argv[1]}`)
@@ -66,8 +51,8 @@ import {Command} from 'commander';
       }),
     };
     fetch(apiUrlPrefix + '/subscriptions', options)
-      .then(response => response.json())
-      .then((data: {index: any}) => {
+      .then((response) => response.json())
+      .then((data: { index: any }) => {
         console.log(data.index);
         successCnt++;
         cb();
