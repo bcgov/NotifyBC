bcf9cc260014331a468d4a72b17e784d3b12ac3c
f-w <fred.wen@gov.bc.ca>
2023-11-10 11:36:21 -0800

    ported home page e2e test

--- notify-bc-lb/src/__tests__/acceptance/home-page.acceptance.ts
+++ notify-bc-lb/src/__tests__/acceptance/home-page.acceptance.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// file ported
 import {Client} from '@loopback/testlab';
 import {setupApplication} from './test-helper';
 
--- test/test-helper.ts
+++ test/test-helper.ts
@@ -1,21 +1,21 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
-import { AppService } from 'src/app.service';
+import { setupApp } from 'src/main';
 import supertest from 'supertest';
 
 export interface AppWithClient {
   app: NestExpressApplication;
   client: supertest.SuperTest<supertest.Test>;
 }
-let app;
+let app: NestExpressApplication;
 export async function setupApplication(): Promise<AppWithClient> {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
 
   app = moduleFixture.createNestApplication<NestExpressApplication>();
-  AppService.app = app;
+  setupApp(app);
   app.enableShutdownHooks();
   await app.init();
   const client = supertest(app.getHttpServer());
