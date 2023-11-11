4a4a1607198b2c027eddd12de60635d01adbb6bd
f-w <fred.wen@gov.bc.ca>
2023-11-11 12:14:33 -0800

    ported admin e2e beforeEach

--- test/administrator.e2e-spec.ts
+++ test/administrator.e2e-spec.ts
@@ -1,12 +1,41 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppConfigService } from 'src/config/app-config.service';
 import supertest from 'supertest';
 import { setupApplication } from './test-helper';
 
-let client: supertest.SuperTest<supertest.Test>;
-beforeAll(async () => {
-  ({ client } = await setupApplication());
-}, 99999);
+describe('Administrator API', () => {
+  let client: supertest.SuperTest<supertest.Test>;
+  let app: NestExpressApplication;
+  const VALID_PASSWORD = '1aA@aaaaaa';
+  beforeAll(async () => {
+    ({ app, client } = await setupApplication());
+  }, 99999);
+  let token: string;
 
-describe('AppController (e2e)', () => {
+  beforeEach(async function () {
+    const appConfig = app.get<AppConfigService>(AppConfigService).get();
+    const origAdminIPs = appConfig.adminIps;
+    appConfig.adminIps = ['127.0.0.1'];
+    let res = await client.post('/api/administrators').send({
+      email: 'foo@invalid.local',
+      password: VALID_PASSWORD,
+    });
+    res = await client
+      .post('/api/administrators/login')
+      .send({
+        email: 'foo@invalid.local',
+        password: VALID_PASSWORD,
+        tokenName: 'myApp',
+      })
+      .set({ is_anonymous: true });
+    token = res.body.token;
+    res = await client.post('/api/administrators').send({
+      email: 'bar@invalid.local',
+      password: VALID_PASSWORD,
+    });
+    appConfig.adminIps = origAdminIPs;
+  });
+
   it('/ (GET)', () => {
     return client.get('/api/administrators/whoami').expect(200);
   });
