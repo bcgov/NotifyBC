156b6e9b02b1efdb272ff89bdd8b59ff7e38c72d
f-w <fred.wen@gov.bc.ca>
2023-11-12 18:22:20 -0800

    PATCH /administrator/{id}/access-tokens

--- test/administrator.e2e-spec.ts
+++ test/administrator.e2e-spec.ts
@@ -343,4 +343,54 @@ describe('Administrator API', () => {
       expect(res.body.id).toHaveLength(64);
     });
   });
+
+  describe('PATCH /administrator/{id}/access-tokens', function () {
+    it('should forbid anonymous user', async function () {
+      const res = await client
+        .patch(`/api/administrators/${id1}/access-tokens`)
+        .send({
+          ttl: 0,
+        });
+      expect(res.status).toEqual(403);
+    });
+    it('should allow super-admin user', async function () {
+      await runAsSuperAdmin(async () => {
+        const res = await client
+          .patch(`/api/administrators/${id1}/access-tokens`)
+          .send({
+            ttl: 0,
+          });
+        expect(res.status).toEqual(200);
+        expect(res.body.count).toEqual(1);
+      });
+    });
+    it('should forbid patching userId', async function () {
+      await runAsSuperAdmin(async () => {
+        const res = await client
+          .patch(`/api/administrators/${id1}/access-tokens`)
+          .send({
+            userId: id2,
+          });
+        expect(res.status).toEqual(403);
+        expect(res.text).toContain('Updating userId is not allowed.');
+      });
+    });
+    it('should allow own record only for admin user', async function () {
+      let res = await client
+        .patch(`/api/administrators/${id2}/access-tokens`)
+        .send({
+          ttl: 0,
+        })
+        .set('Authorization', token);
+      expect(res.status).toEqual(403);
+      res = await client
+        .patch(`/api/administrators/${id1}/access-tokens`)
+        .send({
+          ttl: 0,
+        })
+        .set('Authorization', token);
+      expect(res.status).toEqual(200);
+      expect(res.body.count).toEqual(1);
+    });
+  });
 });
