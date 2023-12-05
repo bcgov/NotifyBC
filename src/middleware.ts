e0f5718c3c87198a39e5c2cecc72ea7e8f652ff1
f-w <fred.wen@gov.bc.ca>
2023-12-04 16:41:21 -0800

    removed serve-favicon

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,11 +26,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as path from 'path';
-
 module.exports = {
   all: {
-    'serve-favicon': { params: [path.join(__dirname, '..', 'favicon.ico')] },
     compression: {},
     helmet: {
       params: [
