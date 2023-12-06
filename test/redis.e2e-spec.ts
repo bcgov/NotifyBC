b194b9f83fe2edfc1181ab0fb2dfcca24c5fc84e
f-w <fred.wen@gov.bc.ca>
2023-12-06 10:33:21 -0800

    added test for CRON clearRedisDatastore

--- test/redis.e2e-spec.ts
+++ test/redis.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import Redis from 'ioredis';
+import { merge } from 'lodash';
 import { RedisMemoryServer } from 'redis-memory-server';
 import { BaseController } from 'src/api/common/base.controller';
 import { NotificationsService } from 'src/api/notifications/notifications.service';
 import { SubscriptionsService } from 'src/api/subscriptions/subscriptions.service';
+import { AppConfigService } from 'src/config/app-config.service';
+import { CronTasksService } from 'src/observers/cron-tasks.service';
 import supertest from 'supertest';
 import { setupApplication, wait } from './test-helper';
 
@@ -11,7 +14,7 @@ let app: NestExpressApplication;
 let client: supertest.SuperTest<supertest.Test>;
 let notificationsService: NotificationsService;
 let subscriptionsService: SubscriptionsService;
-let con, redisServer;
+let con, redisServer, mockedFetch;
 beforeEach(async () => {
   redisServer = new RedisMemoryServer();
   const host = await redisServer.getHost();
@@ -41,36 +44,42 @@ beforeEach(async () => {
 
 afterEach(async () => {
   await con.quit();
-  await BaseController.smsLimiter.disconnect();
+  await BaseController.smsLimiter?.disconnect();
+  delete BaseController.smsLimiter;
   await app.close();
   await redisServer.stop();
 });
 
-describe('POST /notifications', () => {
-  beforeEach(async () => {
-    return Promise.all([
-      subscriptionsService.create({
-        serviceName: 'smsThrottle',
-        channel: 'sms',
-        userChannelId: '12345',
-        state: 'confirmed',
-      }),
-      subscriptionsService.create({
-        serviceName: 'smsThrottle',
-        channel: 'sms',
-        userChannelId: '23456',
-        state: 'confirmed',
-      }),
-    ]);
-  });
+beforeEach(async () => {
+  (
+    BaseController.prototype.sendSMS as unknown as jest.SpyInstance
+  ).mockRestore();
+  mockedFetch = jest.spyOn(global, 'fetch').mockResolvedValue(new Response());
 
+  await Promise.all([
+    subscriptionsService.create({
+      serviceName: 'smsThrottle',
+      channel: 'sms',
+      userChannelId: '12345',
+      state: 'confirmed',
+    }),
+    subscriptionsService.create({
+      serviceName: 'smsThrottle',
+      channel: 'sms',
+      userChannelId: '23456',
+      state: 'confirmed',
+    }),
+    subscriptionsService.create({
+      serviceName: 'smsNoThrottle',
+      channel: 'sms',
+      userChannelId: '12345',
+      state: 'confirmed',
+    }),
+  ]);
+});
+
+describe('POST /notifications', () => {
   it('should throttle sms broadcast push notification with redis', async () => {
-    (
-      BaseController.prototype.sendSMS as unknown as jest.SpyInstance
-    ).mockRestore();
-    const mockedFetch = jest
-      .spyOn(global, 'fetch')
-      .mockResolvedValue(new Response());
     const res = await client
       .post('/api/notifications')
       .send({
@@ -106,5 +115,39 @@ describe('POST /notifications', () => {
       undefined,
     );
     expect(data.length).toEqual(1);
+  });
+});
+
+describe('CRON clearRedisDatastore', () => {
+  it('should update config', async () => {
+    const res = await client
+      .post('/api/notifications')
+      .send({
+        serviceName: 'smsNoThrottle',
+        message: {
+          textBody:
+            'This is a broadcast test {confirmation_code} {service_name} ' +
+            '{http_host} {rest_api_root} {subscription_id} {unsubscription_code}',
+        },
+        channel: 'sms',
+        isBroadcast: true,
+      })
+      .set('Accept', 'application/json');
+    expect(res.status).toEqual(200);
+    let redisRes = await con.hgetall('b_notifyBCSms_settings');
+    expect(redisRes.minTime).toEqual('2000');
+
+    const appConfig = app.get<AppConfigService>(AppConfigService).get();
+    const origSmsConfig = appConfig.sms;
+    const newSmsConfig = merge({}, origSmsConfig, {
+      throttle: { minTime: 1000 },
+    });
+    appConfig.sms = newSmsConfig;
+
+    const cronTasksService = app.get<CronTasksService>(CronTasksService);
+    await cronTasksService.clearRedisDatastore()();
+    redisRes = await con.hgetall('b_notifyBCSms_settings');
+    expect(redisRes.minTime).toEqual('1000');
+    appConfig.sms = origSmsConfig;
   });
 });
