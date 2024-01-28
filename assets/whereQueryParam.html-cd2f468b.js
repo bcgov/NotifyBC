44179a198865d12e5fda36f143136df3a5cf3056
f-w <f-w@users.noreply.github.com>
2024-01-28 20:13:29 +0000

    Deploying to gh-pages from  @ 0095cc815b6d8a78a35b81975f19261884be1abf 🚀

---
+++ assets/whereQueryParam.html-cd2f468b.js
@@ -0,0 +1,10 @@
+import{_ as n,r as o,o as a,c,a as e,b as r,d as s}from"./app-bb9434ef.js";const l={},m=e("em",null,"where",-1),d={href:"https://www.mongodb.com/docs/manual/tutorial/query-documents/",target:"_blank",rel:"noopener noreferrer"},u=e("pre",null,[e("code",null,`- parameter name: where
+- required: false
+- parameter type: query
+- data type: object
+
+The value can be expressed as either
+
+  1. URL-encoded stringified JSON object (see example below); or
+  2. in the format supported by [qs](https://github.com/ljharb/qs), for example \`?where[created][$gte]="2023-01-01"&where[created][$lt]="2024-01-01"\`
+`)],-1);function h(p,_){const t=o("ExternalLinkIcon");return a(),c("div",null,[e("p",null,[r("a "),m,r(" query parameter with value conforming to MongoDB "),e("a",d,[r("Query Documents"),s(t)])]),u])}const f=n(l,[["render",h],["__file","whereQueryParam.html.vue"]]);export{f as default};
