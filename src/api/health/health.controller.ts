b5755250b4bb9314172aa21ce4d399d1b14eaaa6
f-w <fred.wen@gov.bc.ca>
2024-05-01 14:44:46 -0700

    added health check for mongodb

--- src/api/health/health.controller.ts
+++ src/api/health/health.controller.ts
@@ -1,11 +1,14 @@
 import {Controller, Get} from '@nestjs/common';
-import {HealthCheck, HealthCheckService, HttpHealthIndicator} from '@nestjs/terminus';
+import {
+  HealthCheck, HealthCheckService, HttpHealthIndicator, MongooseHealthIndicator
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private mongoose: MongooseHealthIndicator,
   ) {}
 
   @Get()
@@ -13,6 +16,7 @@ export class HealthController {
   check() {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.mongoose.pingCheck('MongoDB'),
     ]);
   }
 }
