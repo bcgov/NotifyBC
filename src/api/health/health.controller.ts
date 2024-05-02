a15563189bb9d216e1ffc77409f960c1eedb906e
f-w <fred.wen@gov.bc.ca>
2024-05-01 17:17:55 -0700

    added redis health check

--- src/api/health/health.controller.ts
+++ src/api/health/health.controller.ts
@@ -4,7 +4,9 @@ import {
   HealthCheckService,
   MongooseHealthIndicator,
 } from '@nestjs/terminus';
+import { AppConfigService } from 'src/config/app-config.service';
 import { ConfigHealthService } from './config-health.service';
+import { RedisHealthService } from './redis-health.service';
 
 @Controller('health')
 export class HealthController {
@@ -12,14 +14,23 @@ export class HealthController {
     private health: HealthCheckService,
     private mongoose: MongooseHealthIndicator,
     private configHealthService: ConfigHealthService,
+    private appConfigService: AppConfigService,
+    private redisHealthService: RedisHealthService,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([
+    const checks = [
       () => this.mongoose.pingCheck('MongoDB'),
       () => this.configHealthService.isHealthy('config'),
-    ]);
+    ];
+    if (
+      this.appConfigService.get('email.throttle.datastore') == 'ioredis' ||
+      this.appConfigService.get('sms.throttle.datastore') == 'ioredis'
+    ) {
+      checks.push(() => this.redisHealthService.isHealthy('redis'));
+    }
+    return this.health.check(checks);
   }
 }
--- src/api/health/health.module.ts
+++ src/api/health/health.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { ConfigModule } from 'src/config/config.module';
 import { ConfigurationsModule } from '../configurations/configurations.module';
 import { ConfigHealthService } from './config-health.service';
 import { HealthController } from './health.controller';
+import { RedisHealthService } from './redis-health.service';
 
 @Module({
-  imports: [TerminusModule, ConfigurationsModule],
+  imports: [TerminusModule, ConfigurationsModule, ConfigModule],
   controllers: [HealthController],
-  providers: [ConfigHealthService],
+  providers: [ConfigHealthService, RedisHealthService],
 })
 export class HealthModule {}
