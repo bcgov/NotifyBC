d53c30940a16c4b457e07d86db483a032613f191
f-w <fred.wen@gov.bc.ca>
2024-05-13 11:35:23 -0700

    added memory dump

---
+++ src/api/memory/memory.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { writeHeapSnapshot } from 'node:v8';
+import { Role } from 'src/auth/constants';
+import { Roles } from 'src/auth/roles.decorator';
+import { MemoryService } from './memory.service';
+
+@Controller('memory')
+@Roles(Role.SuperAdmin)
+export class MemoryController {
+  constructor(private readonly memoryService: MemoryService) {}
+
+  @Get()
+  dumpMemory(@Query('fileName') fileName?: string) {
+    // Generate a heapdump for the main thread.
+    return writeHeapSnapshot(fileName);
+  }
+}
