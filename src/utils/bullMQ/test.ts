555323b7067c19615e82a8c19166a5575a9ea432
f-w <fred.wen@gov.bc.ca>
2024-10-26 16:08:35 -0700

    queueEvents

--- src/utils/bullMQ/test.ts
+++ src/utils/bullMQ/test.ts
@@ -1,4 +1,4 @@
-import { Queue, Worker } from 'bullmq';
+import { Queue, QueueEvents, Worker } from 'bullmq';
 
 const myQueue = new Queue('foo', {
   connection: {
@@ -17,19 +17,40 @@ const myQueue = new Queue('foo', {
 
 async function addJobs() {
   for (let i = 0; i < 10; i++) {
-    await myQueue.add('myJobName', {
+    async function theWork() {
+      console.log({ count: i, time: new Date() });
+    }
+    const queueEvents = new QueueEvents('foo');
+    const queuedID = [];
+    // IMPORTANT: place queueEvents.on before myQueue.add
+    queueEvents.on('completed', ({ jobId }) => {
+      if (!j?.id) {
+        queuedID.push(jobId);
+        return;
+      }
+      if (jobId !== j?.id) {
+        return;
+      }
+      theWork();
+      queueEvents.close();
+    });
+    const j = await myQueue.add('myJobName', {
       count: i,
     });
+    // extra guard in case queueEvents.on is called before j is assigned.
+    if (queuedID.indexOf(j.id) >= 0) {
+      theWork();
+    }
   }
 }
 
 addJobs();
 new Worker(
   'foo',
-  async (job) => {
+  async () => {
     // Will print { foo: 'bar'} for the first job
     // and { qux: 'baz' } for the second.
-    console.log({ ...job.data, time: new Date() });
+    // console.log({ ...job.data, time: new Date() });
   },
   {
     connection: {
