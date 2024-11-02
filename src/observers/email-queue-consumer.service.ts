3701916ccb323195e1aecf8217c079b0f34e941b
f-w <fred.wen@gov.bc.ca>
2024-11-02 15:07:57 -0700

    updated rateLimit; still not working

--- src/observers/email-queue-consumer.service.ts
+++ src/observers/email-queue-consumer.service.ts
@@ -3,7 +3,12 @@ import { Injectable } from '@nestjs/common';
 import { Job } from 'bullmq';
 
 @Injectable()
-@Processor('e')
+@Processor('e', {
+  limiter: {
+    max: 1,
+    duration: 10000,
+  },
+})
 export class EmailQueueConsumerService extends WorkerHost {
   async process(job: Job<any, any, string>) {
     console.log(job.id);
