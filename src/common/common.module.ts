484e04e25c534b4d23df23342d927c7eafc315a2
f-w <fred.wen@gov.bc.ca>
2024-11-25 12:42:35 -0800

    moved updateBroadcastPushNotificationStatus to commonService

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,6 +1,7 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Global, Module } from '@nestjs/common';
 import { BouncesModule } from 'src/api/bounces/bounces.module';
+import { NotificationsModule } from 'src/api/notifications/notifications.module';
 import { ConfigurationsModule } from '../api/configurations/configurations.module';
 import { ConfigModule } from '../config/config.module';
 import { CommonService } from './common.service';
@@ -16,6 +17,7 @@ import { CommonService } from './common.service';
       { name: 'n' /* notification */ },
     ),
     BouncesModule,
+    NotificationsModule,
   ],
   providers: [CommonService],
   exports: [CommonService],
