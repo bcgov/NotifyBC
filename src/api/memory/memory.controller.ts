db424d2ff9d7bb2bfe8e87a8110bfa74a15f9f55
f-w <fred.wen@gov.bc.ca>
2024-12-13 15:57:53 -0800

    exclude memory from OAS3

--- src/api/memory/memory.controller.ts
+++ src/api/memory/memory.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import { writeHeapSnapshot } from 'node:v8';
 import { Role } from 'src/auth/constants';
 import { Roles } from 'src/auth/roles.decorator';
 import { MemoryService } from './memory.service';
 
 @Controller('memory')
+@ApiExcludeController()
 @Roles(Role.SuperAdmin)
 export class MemoryController {
   constructor(private readonly memoryService: MemoryService) {}
