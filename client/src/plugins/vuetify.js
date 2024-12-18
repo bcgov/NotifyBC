020cd74c8129ce920889ab93afca0fe38d0e74e3
f-w <fred.wen@gov.bc.ca>
2024-12-18 11:20:53 -0800

    support dark mode

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -11,9 +11,17 @@ import 'vuetify/styles';
 // Composables
 import { createVuetify } from 'vuetify';
 
+const defaultTheme =
+  localStorage.getItem('notifyBcWebConsoleTheme') ??
+  (window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light');
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme,
     themes: {
       light: {
         colors: {
