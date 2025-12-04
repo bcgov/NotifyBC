f376bc1b5645dec3373c7207d2748acafdf1d61f
f-w <f-w@users.noreply.github.com>
2025-12-04 17:26:41 +0000

    Deploying to gh-pages from  @ a4959b68685fa82ecec43073259f4bc8b75efd30 🚀

---
+++ preview/assets/filterQueryParamExample.html-DmAGhqxm.js
@@ -0,0 +1,11 @@
+import{_ as t,c as r,a as e,o as n}from"./app-IsMqn1kS.js";const o={};function l(c,a){return n(),r("div",null,[...a[0]||(a[0]=[e("p",null,"the value of the filter query parameter is URL-encoded stringified JSON object",-1),e("pre",null,[e("code",null,`\`\`\`json
+{
+  "where": {
+    "created": {
+      "$gte": "2023-01-01",
+      "$lt": "2024-01-01"
+    }
+  }
+}
+\`\`\`
+`)],-1)])])}const m=t(o,[["render",l]]),i=JSON.parse('{"path":"/docs/shared/filterQueryParamExample.html","title":"","lang":"en-US","frontmatter":{},"git":{"contributors":[{"name":"f-w","username":"f-w","email":"fred.wen@gov.bc.ca","commits":1,"url":"https://github.com/f-w"}],"changelog":[{"hash":"a4959b68685fa82ecec43073259f4bc8b75efd30","time":1764868318000,"email":"fred.wen@gov.bc.ca","author":"f-w","message":"updated version"}]},"filePathRelative":"docs/shared/filterQueryParamExample.md"}');export{m as comp,i as data};
--- preview/assets/whereQueryParamExample.html-CAvGvl6L.js
+++
@@ -1,9 +0,0 @@
-import{_ as r,c as n,a as e,e as t,o}from"./app-oAluNbDS.js";const c={};function s(l,a){return o(),n("div",null,[...a[0]||(a[0]=[e("p",null,[t("the value of the "),e("em",null,"where"),t(" query parameter is URL-encoded stringified JSON object")],-1),e("pre",null,[e("code",null,`\`\`\`json
-{
-  "created": {
-    "$gte": "2023-01-01",
-    "$lt": "2024-01-01"
-  }
-}
-\`\`\`
-`)],-1)])])}const d=r(c,[["render",s]]),f=JSON.parse('{"path":"/docs/shared/whereQueryParamExample.html","title":"","lang":"en-US","frontmatter":{},"git":{"contributors":[{"name":"f-w","username":"f-w","email":"fred.wen@gov.bc.ca","commits":1,"url":"https://github.com/f-w"}],"changelog":[{"hash":"1c83a1ff6622b7937ab3d57f93ffce55d055bbc0","time":1764868318000,"email":"fred.wen@gov.bc.ca","author":"f-w","message":"updated version"}]},"filePathRelative":"docs/shared/whereQueryParamExample.md"}');export{d as comp,f as data};
