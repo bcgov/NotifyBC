9d09b22a11c37c6d2f52a6ffb41b7da2b2ef8d14
f-w <f-w@users.noreply.github.com>
2025-12-04 17:17:07 +0000

    Deploying to gh-pages from  @ 1c83a1ff6622b7937ab3d57f93ffce55d055bbc0 🚀

---
+++ preview/assets/whereQueryParam.html-HMiBKkyn.js
@@ -0,0 +1,10 @@
+import{_ as t,c as o,a as e,e as a,o as n}from"./app-oAluNbDS.js";const s={};function c(m,r){return n(),o("div",null,[...r[0]||(r[0]=[e("p",null,[a("a "),e("em",null,"where"),a(" query parameter with value conforming to MongoDB "),e("a",{href:"https://www.mongodb.com/docs/manual/tutorial/query-documents/",target:"_blank",rel:"noopener noreferrer"},"Query Documents")],-1),e("pre",null,[e("code",null,`- parameter name: where
+- required: false
+- parameter type: query
+- data type: object
+
+The value can be expressed as either
+
+  1. URL-encoded stringified JSON object (see example below); or
+  2. in the format supported by [qs](https://github.com/ljharb/qs), for example \`?where[created][$gte]="2023-01-01"&where[created][$lt]="2024-01-01"\`
+`)],-1)])])}const d=t(s,[["render",c]]),u=JSON.parse('{"path":"/docs/shared/whereQueryParam.html","title":"","lang":"en-US","frontmatter":{},"git":{"contributors":[{"name":"f-w","username":"f-w","email":"fred.wen@gov.bc.ca","commits":1,"url":"https://github.com/f-w"}],"changelog":[{"hash":"1c83a1ff6622b7937ab3d57f93ffce55d055bbc0","time":1764868318000,"email":"fred.wen@gov.bc.ca","author":"f-w","message":"updated version"}]},"filePathRelative":"docs/shared/whereQueryParam.md"}');export{d as comp,u as data};
