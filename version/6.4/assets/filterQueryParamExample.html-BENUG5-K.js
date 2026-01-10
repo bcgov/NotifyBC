079967a951558cc9d4f315b0f57c3e205a76eafc
f-w <f-w@users.noreply.github.com>
2026-01-10 20:44:21 +0000

    Deploying to gh-pages from  @ eda6f96a07369fa623a5dda5e635982c34ea95fc 🚀

---
+++ version/6.4/assets/filterQueryParamExample.html-BENUG5-K.js
@@ -0,0 +1,11 @@
+import{_ as t,c as r,a as e,o as n}from"./app-B4UY22Qp.js";const o={};function l(c,a){return n(),r("div",null,[...a[0]||(a[0]=[e("p",null,"the value of the filter query parameter is URL-encoded stringified JSON object",-1),e("pre",null,[e("code",null,`\`\`\`json
+{
+  "where": {
+    "created": {
+      "$gte": "2023-01-01",
+      "$lt": "2024-01-01"
+    }
+  }
+}
+\`\`\`
+`)],-1)])])}const m=t(o,[["render",l]]),d=JSON.parse('{"path":"/docs/shared/filterQueryParamExample.html","title":"","lang":"en-US","frontmatter":{},"git":{"contributors":[{"name":"f-w","username":"f-w","email":"fred.wen@gov.bc.ca","commits":1,"url":"https://github.com/f-w"}],"changelog":[{"hash":"eda6f96a07369fa623a5dda5e635982c34ea95fc","time":1768075000000,"email":"fred.wen@gov.bc.ca","author":"f-w","message":"updated docs"}]},"filePathRelative":"docs/shared/filterQueryParamExample.md"}');export{m as comp,d as data};
